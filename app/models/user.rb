e0d5bedc7f711240da1cbc9f4adb5f692f242ca5
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-11-21 20:52:52 -0600

    Completed through step 12

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :presence => true, :uniqueness => true
+  # validates :email, presence => true
+  # validates :encrypted_password, presence => true
+
+  has_many :photos
+  has_many :comments
+  has_many :likes
+  has_many :liked_photos, :through => :likes, :source => :photo
+
+end
